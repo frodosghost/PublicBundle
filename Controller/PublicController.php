ba9c7150940c01b8d7cccdd0572b4ce369e9c9c2
James Rickard <james@frodosghost.com>
2013-08-28 08:40:50 +1000

    Added controller actions for Contact form display and success. Added routes and templates for views.

--- Controller/PublicController.php
+++ Controller/PublicController.php
@@ -4,10 +4,73 @@ namespace Manhattan\PublicBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Manhattan\PublicBundle\Entity\Contact;
+use Manhattan\PublicBundle\Form\ContactType;
+
 class PublicController extends Controller
 {
     public function homeAction()
     {
         return $this->render('ManhattanPublicBundle:Public:home.html.twig');
+    }
+
+    /**
+     * Contact Us Page
+     *
+     * @param  Request $request
+     * @return Response
+     */
+    public function contactAction(Request $request)
+    {
+        $contact = new Contact;
+
+        $form = $this->createForm(new ContactType(), $contact, array(
+            'render_fieldset' => false
+        ));
+
+        return $this->render('ManhattanPublicBundle:Contact:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    public function contactSendAction(Request $request)
+    {
+        $contact = new Contact;
+
+        $form = $this->createForm(new ContactType(), $contact, array(
+            'render_fieldset' => false
+        ));
+
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $data = $form->getData();
+
+            // Check the Honeypot field is not set.
+            if ($data->isHuman()) {
+                $this->get('manhattan.mailer')->sendContactEmail($data);
+
+                $this->get('session')->getFlashBag()->add('notice', 'Your contact email has been sent.');
+                return $this->redirect($this->generateUrl('contact_success'));
+            }
+        }
+
+        return $this->render('ManhattanPublicBundle:Contact:contact.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * Displays Success Page
+     *
+     * @return Response
+     */
+    public function contactSuccessAction()
+    {
+        if (!$this->get('session')->getFlashBag()->has('notice')) {
+            return $this->redirect($this->generateUrl('contact'));
+        }
+
+        return $this->render('ManhattanPublicBundle:Contact:success.html.twig');
     }
 }
