fa9f88529bd6868922ee6418fd4c0d7f96c59f49
James Rickard <james@frodosghost.com>
2013-09-04 13:33:04 +1000

    Added Sitemap functions to setup to allow display of Posts and Pages.

--- Controller/PublicController.php
+++ Controller/PublicController.php
@@ -83,4 +83,42 @@ class PublicController extends Controller
 
         return $this->render('ManhattanPublicBundle:Contact:success.html.twig');
     }
+
+    /**
+     * Displays the Sitemap
+     */
+    public function sitemapAction(Request $request)
+    {
+        $format = $request->getRequestFormat();
+
+        if (!in_array($format, array('html', 'xml'))) {
+            throw $this->createNotFoundException(sprintf('Exception: 404 Page Not Found. Unable to find page with URI: "%s"', $this->getRequest()->getUri()));
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $controller = $this;
+
+        $htmlTree = $em->getRepository('ManhattanPublicBundle:Page')
+            ->setPublishState($this->container->getParameter('manhattan.constant.publish'))
+            ->findPublishedNodesForDisplay(array('decorate' => false));
+
+        $articles = $em->getRepository('ManhattanPublicBundle:Article\Article')->findAllArticles();
+
+        if ($format == 'html') {
+            return $this->render('ManhattanPublicBundle:Sitemap:sitemap.html.twig', array(
+                'tree'        => $htmlTree,
+                'articles'    => $articles,
+            ));
+        }
+
+        $xml_content = $this->renderView('ManhattanPublicBundle:Sitemap:sitemap.xml.twig', array(
+            'tree'        => $htmlTree,
+            'articles'    => $articles,
+        ));
+
+        $response = new Response($xml_content);
+        $response->headers->set('Content-Type', 'text/xml');
+
+        return $response;
+    }
 }
